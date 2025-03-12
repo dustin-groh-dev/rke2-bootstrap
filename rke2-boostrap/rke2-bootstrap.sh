87fb16ddc88282a9b1820ff4f124e2ee73cd50f2
Dustin Groh <dustingroh33@gmail.com>
2025-03-12 13:37:13 -0400

    Update rke2-bootstrap.sh
    added 
    - section to check that the rke2-server service starts on the first node.
    - section to check that the path for the node token gets created

--- rke2-boostrap/rke2-bootstrap.sh
+++ rke2-boostrap/rke2-bootstrap.sh
@@ -67,32 +67,44 @@ echo "Processing node: $server_node"
 
 echo " "
 
-ssh -i "$server_key" "$node_user"@"$server_node" <<OUTER_EOF
+ssh -i "$server_key" "$node_user@$server_node" <<OUTER_EOF
 echo "Running commands on server node $server_node ..."
 
 # Download RKE2 binary
 curl -sfL https://get.rke2.io | sudo INSTALL_RKE2_VERSION=$RKE2_VERSION sh -
 
-# Enable and start RKE2 server
+# Enable and start RKE2 server service
 sudo systemctl enable rke2-server.service
 sudo systemctl start rke2-server.service
 
+# test if rke2-server service activates, sleep for 5 seconds if it doesnt
+echo " "
+echo "Waiting for rke2-server service to start..."
+until sudo systemctl is-active --quiet rke2-server.service; do
+  echo "rke2-server service is not active yet. Retrying..."
+  sleep 5
+done
+
+echo "rke2-server service is active."
+
+# Wait for node-token to be created, sleep for 5 seconds if it's not created yet
+echo "Waiting for node-token to be available..."
+until sudo test -f /var/lib/rancher/rke2/server/node-token; do
+  echo "node-token not found, waiting..."
+  sleep 5
+  ls -l /var/lib/rancher/rke2/server/node-token || echo "Still no node-token."
+done
+
+echo "node-token found."
 OUTER_EOF
 
-# grab the token from the first server node
+# Grab the token after confirming the file exists
 TOKEN=$(ssh -T -o BatchMode=yes -i "$server_key" "$node_user@$server_node" "sudo cat /var/lib/rancher/rke2/server/node-token")
-
-
-# Check if the token was successfully fetched
-if [[ -z "$TOKEN" ]]; then
-    echo "Failed to fetch the node token. Exiting."
-    exit 1
-fi
+echo "Token retrieved successfully."
 
 # Print the fetched token
 echo " "
 echo "Fetched node token: $TOKEN"
-
 
 # Process the other nodes
 for ip in "${!nodes[@]}"; do
@@ -134,6 +146,16 @@ echo " "
 # Enable and start RKE2 agent
 sudo systemctl enable rke2-server.service
 sudo systemctl start rke2-server.service
+
+# test if service activates
+echo " "
+echo "Waiting for rke2-server service to start..."
+until sudo systemctl is-active --quiet rke2-server.service; do
+  echo "rke2-server service is not active yet. Retrying..."
+  sleep 5
+done
+
+echo "rke2-server service is active."
 
 OUTER_EOF
 
@@ -180,6 +202,7 @@ check_nodes_ready() {
 # Loop until all nodes are ready
 echo "Checking node readiness..."
 echo " "
+
 while true; do
     if check_nodes_ready; then
         echo " "
